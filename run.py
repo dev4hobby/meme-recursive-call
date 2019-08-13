bd57f48845b00eadbc44cae0455b2b9560cb21bd
Seongcheul, Ahn <aciddust20@gmail.com>
2019-08-13 14:51:04 +0900

    Add, play with tabspace

--- run.py
+++ run.py
@@ -34,20 +34,29 @@ def main():
         Datas[0] = intro_fd.read()
     with open(filepath[1], 'r') as answer_fd:
         Datas[1] = answer_fd.read()
+        while Datas[1][-1] == '\n':
+            Datas[1] = Datas[1][:-2]
     
     # The beginning of the legend.
     for character in Datas[0]:
         print(character, end='', flush=True)
         time.sleep(0.1)
-    what_is_recursive_function(count)
+    what_is_recursive_function(count, 0)
 
-def what_is_recursive_function(remained):
+def what_is_recursive_function(remained, tabcount):
+    print('\t'*tabcount, end='', flush=True)
     for character in Datas[1]:
-        print(character, end='', flush=True)
-        time.sleep(0.1)
+        if(character == '\n'):
+            print('\n', end='', flush=True)
+            print('\t'*tabcount, end='', flush=True)
+            time.sleep(0.1)
+        else:
+            print(character, end='', flush=True)
+            time.sleep(0.1)
     remained -= 1
     if remained > 0:
-        what_is_recursive_function(remained)
+        print('')
+        what_is_recursive_function(remained, tabcount+1)
     else:
         print('...')
 
